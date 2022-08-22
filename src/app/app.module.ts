0b0dec01c339c1fa77a0b4fcdfaf2cbed13ef61e
Karina Borges <karina@cuponeria.com.br>
2022-08-21 22:19:13 -0300

    ✨ Create product delete component and Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 @NgModule({
@@ -45,6 +46,7 @@ registerLocaleData(localePt);
     ProductCreateComponent,
     ProductReadComponent,
     ProductUpdateComponent,
+    ProductDeleteComponent,
   ],
   imports: [
     BrowserModule,
